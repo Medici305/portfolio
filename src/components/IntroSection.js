0c838025dd3f1c17e9e1c18b4566eb2f928aa813
unknown <Medici305>
2021-04-30 18:57:05 +0100

    11.6 react-lazy loading images

--- src/components/IntroSection.js
+++ src/components/IntroSection.js
@@ -1,12 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 import { makeStyles } from "@material-ui/core/styles";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 import Grid from "@material-ui/core/Grid";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import Button from "@material-ui/core/Button";
-import ProgressiveImage from "react-progressive-image";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import Code from "../img/pexels-negative-space-169573.jpg";
-import CompressedCode from "../img/compressed-intro.jpg";
+// import CompressedCode from "../img/compressed-intro.jpg";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -65,11 +67,14 @@ const IntroSection = ({ myRef }) => {
     <Intro className={classes.root}>
       <Grid container className={classes.box}>
         <Grid item className={classes.image} xs={12}>
-          <ProgressiveImage src={CompressedCode} placeholder="tiny-image.jpg">
-            {(src, loading) => (
-              <img style={{ opacity: .9 }} src={Code} alt="an image" />
-            )}
-          </ProgressiveImage>
+          <LazyLoadImage
+            alt="code"
+            effect="blur"
+            src={Code}
+            height="60vh"
+            width="100%"
+            placeholderSrc={<CircularProgress />} // use normal <img> attributes as props
+          />
         </Grid>
         <Grid item className={classes.text} sm={8} lg={5}>
           <h1>Hey, I'm Julien Osman and I love building beautiful websites.</h1>
